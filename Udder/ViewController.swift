bfe0aa231b74f89517de3779c725d1d435b43827
CantKillemAll <thierry.dilenarda@gmail.com>
2019-06-19 11:53:52 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Thierry on 19/06/2019.
+//  Copyright © 2019 Thierry Di Lenarda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
